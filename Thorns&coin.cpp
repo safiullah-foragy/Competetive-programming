0450395f71ef22911b460d9a3b9cdc9f97708949
HP <safiullahforagy.com>
2024-06-26 14:43:27 +0600

    just solved

---
+++ Thorns&coin.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int ind=-1;
+     int coin=0;
+    for(int i=0;i<n;i++)
+    {
+         if(s[i]=='@'){coin++;}
+if((s[i]=='.'||s[i]=='@')&&(s[i+1]=='*'&&s[i+2]=='*')){break;}
+ 
+    }
+
+   
+   
+    cout<<coin<<endl;
+  }
+  return 0;
+}
