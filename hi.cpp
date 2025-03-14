50a07ba5545814d724dfbcfb60d5ad125b1d67f9
safiullah-foragy <safiullahforagy@gmail.com>
2025-03-14 23:52:07 +0600

    Initial commit

---
+++ hi.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+void solve()
+{
+
+  
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
